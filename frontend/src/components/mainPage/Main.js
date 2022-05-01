92c7de52ffc17981ac9e723eedd21571ee2a3d56
hugo <hugosousa76@gmail.com>
2022-05-01 19:43:45 -0300

    added user info on the sidebar

--- frontend/src/components/mainPage/Main.js
+++ frontend/src/components/mainPage/Main.js
@@ -5,21 +5,43 @@ import './style.css';
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
+        this.state = {      
+            forms: []//você pode inicializar state com dados manualmente ou com um array vazio caso os dados sejam carregados via API
+        };
+    
+  }
+
+  async getForms() {
+    const response = await fetch("http://localhost:9000/getForms", { credentials: 'include' });
+    const data = await response.json();
+    const userData = data[0]
+    this.state = {
+      fullname: userData.fullname,
+      username: userData.username,
+      email: userData.email,
+    }
+    console.log(userData)
+    console.log(`O state é ${this.state}`)
+            this.setState(state => ({
+                forms: userData
+            }));
   }
 
   logout(props) {
-    fetch('http://localhost:9000/logout', {credentials: 'include'})
-    .then(response => response.json())
-    .then(data => console.log(data))
+    fetch('http://localhost:9000/logout', { credentials: 'include' })
+      .then(response => response.json())
+      .then(data => console.log(data))
     props.changeState('login')
   }
 
   render() {
     return (
-      <div className="Layout">
+      <div className="Layout" onLoad={this.getForms()}>
         <div className="user--panel">
           <div className="prof--picture"></div>
-          <span className="username">Malaquias</span>
+          <span className="username">{this.state.forms.fullname}</span>
+          <span className="username">{this.state.forms.username}</span>
+          <span className="username">{this.state.forms.email}</span>
           <div className="user--menu">
             <button className="activebtn">Dashboard</button>
             <button onClick={() => this.props.changeState('athosPage')}>Edit Profile</button>
