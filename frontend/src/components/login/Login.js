e028d9016c89e4c21c2d95dc06c2b5ab298edecc
gab1005 <gabrielqwerty1@gmail.com>
2022-05-08 18:40:21 -0300

    criando nova branch e cirando elementos html sem jsx, apenas com React.createElement

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,6 +1,46 @@
 import React from "react";
-
 import './style.css'
+
+import Base from './BaseLogin';
+
+const props01 = [
+  {spellCheck: 'false'}, 
+  {type:'text'},
+  {className:'input--field'},
+  {placeholder:'Username or email'}, 
+  {value: this.state.identifier}, 
+  // {onChange: this.handleIdentifierChange}
+];
+
+const props02 = [
+  {spellCheck:'false'}, 
+  {type:'password'}, 
+  {className:'input--field'}, 
+  {placeholder:'Password'}, 
+  // {onChange:this.handlePasswordChange},
+];
+
+const props03 = [
+  {type:'submit'}, 
+  {className:'input--submit'}, 
+  {value:'Login'}
+];
+
+const props04 = [
+  {className:'has--account'},
+  // {onClick: () => this.props.changeState('register') }
+];
+
+//
+// let elem = React.createElement(
+//   this.props.tag,
+//   { 
+//       style: this.props.style,
+//       id: this.props.id
+//       onClick: () => {console.log('clicked')}
+//   },
+// )
+//
 
 export default class Login extends React.Component {
       constructor(props) {
@@ -49,21 +89,22 @@ export default class Login extends React.Component {
         })
     }
 
+
+    
   render() {
     return (
-      <div className="Layout">
-        <div className='login--panel'>
-          <h1 className='login--title'>
-            MyHashProfiler
-          </h1>
-          <form onSubmit={this.handleSubmit}>
-            <input spellCheck='false' type={'text'} className='input--field' placeholder='Username or email' value={this.state.identifier} onChange={this.handleIdentifierChange}></input>
-            <input spellCheck='false' type={'password'} className='input--field' placeholder='Password' onChange={this.handlePasswordChange}></input>
-            <input type={'submit'} className='input--submit' value={'Login'} ></input>
-          </form>
-            <span className='has--account' onClick={() => this.props.changeState('register')}>Create account</span>
-        </div>
-      </div>
+      // <Base />
+      React.createElement('div',{className:'Layout'},
+        React.createElement('div', {className:'login--panel'},
+            React.createElement('h1', {className: 'login--title'}, 'MyHashProfiler'),
+            React.createElement('form', {onSubmit: this.handleSubmit}, 
+                React.createElement('input', props01, null),
+                React.createElement('input', props02, null),
+                React.createElement('input', props03, null),
+            ),
+            React.createElement('span', props04, 'Create account')
+        )
+      )
     );
   }
 };
