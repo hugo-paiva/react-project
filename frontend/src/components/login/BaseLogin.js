3acc83bfcc7cf8d826c8bf781d6f9b75d5c6aa0d
gab1005 <gabrielqwerty1@gmail.com>
2022-05-11 16:36:17 -0300

    finalizado, mas sem css

--- frontend/src/components/login/BaseLogin.js
+++ frontend/src/components/login/BaseLogin.js
@@ -2,33 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';   
 
-const props01 = [
-    {spellCheck: "false"}, 
-    {type:'text'},
-    {className:'input--field'},
-    {placeholder:'Username or email'}, 
-    {value: "this.state.identifier"}, 
-    {onChange: "this.handleIdentifierChange"},
-];
+const props01 = {
+    spellCheck: "false", 
+    type:'text',
+    placeholder:'Username or email', 
+    value: "this.state.identifier", 
+    onChange: "this.handleIdentifierChange",
+};
 
-const props02 = [
-    {spellCheck: "false"}, 
-    {type:'password'}, 
-    {className:'input--field'}, 
-    {placeholder:'Password'}, 
-    {onChange:"this.handlePasswordChange"},
-];
+const props02 = {
+    spellCheck: "false", 
+    type:'password', 
+    placeholder:'Password', 
+    onChange:"this.handlePasswordChange",
+};
 
-const props03 = [
-    {type:'submit'}, 
-    {className:'input--submit'}, 
-    {value: 'Login'},
-];
+const props03 = {
+    type:'submit', 
+    value: 'Login',
+};
 
-const props04 = [
-    {className:'has--account'},
-    {onClick: () => this.props.changeState('register')}
-];
+const props04 = {
+    className:'has--account',
+    onClick: () => this.props.changeState('register')
+};
 
 export default class BaseLogin extends React.Component{
     render() {
@@ -37,9 +34,9 @@ export default class BaseLogin extends React.Component{
                 React.createElement('div', {className:'login--panel'},
                     React.createElement('h1', {className: 'login--title'}, 'MyHashProfiler'),
                     React.createElement('form', {onSubmit: "this.handleSubmit"}, 
-                        React.createElement('input', props01,  null),
-                        React.createElement('input', props02,  null),
-                        React.createElement('input', props03,  null)
+                        React.createElement('input', [{className:'input--field'}, props01],  null),
+                        React.createElement('input', [{className:'input--field'}, props02],  null),
+                        React.createElement('input', [{className:'input--submit'}, props03],  null)
                     ),
                     React.createElement('span', props04, 'Create account')
                     )
@@ -49,6 +46,8 @@ export default class BaseLogin extends React.Component{
 } 
 
 {/**
+{className:'has--account', onClick: () => this.props.changeState('register')}
+
 return (
     React.createElement('div',{className:'Layout'},
         React.createElement('div', {className:'login--panel'},
